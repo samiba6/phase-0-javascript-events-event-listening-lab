807da0e7fd1c808c4660b3e0237ba1314c209e82
Sami <s.babouche@esi-sba.dz>
2023-09-13 01:54:04 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const button = document.getElementById('button');
+
 function addingEventListener() {
+
+
+    button.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
 }
+addingEventListener()
